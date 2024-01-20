71776ab903ae366e7a03ae9d5d79a0d6097d418b
Jakub Muszyński <83725776+Mushelox@users.noreply.github.com>
2024-01-20 12:38:49 +0100

    Drop items reordering

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using PdfApp;
 using MudBlazor.Services;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+             .WriteTo.Console()
+             .MinimumLevel.Information()
+             .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+             .Enrich.FromLogContext()
+             .CreateLogger();
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
