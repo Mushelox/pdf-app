a5288191a13aaf84ea3d8640900b9535b0ba2773
Jakub Muszyński <83725776+Mushelox@users.noreply.github.com>
2024-02-11 13:45:16 +0100

    Working pdf generation and display pipeline

---
+++ Shared/Services/PdfService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.JSInterop;
+using MudBlazor;
+using PdfApp.Shared.Models.PdfBuilder;
+using PdfApp.Shared.Models.PdfBuilder.Elements;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace PdfApp.Shared.Services;
+
+public class PdfService
+{
+    private readonly IJSRuntime _jsRuntime;
+    private readonly ILogger<PdfService> _logger;
+
+    public PdfService(IJSRuntime jsRuntime, ILogger<PdfService> logger)
+    {
+        _jsRuntime = jsRuntime;
+        _logger = logger;
+    }
+
+    public async Task GenerateAndDisplayPdf(IEnumerable<PdfElement> pdfElements)
+    {
+        byte[] pdfBytes = GeneratePdf(pdfElements);
+        string pdfBase64 = Convert.ToBase64String(pdfBytes);
+
+        var pdfUtilityJsModule = await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "./scripts/pdfServiceUtility.js");
+        await pdfUtilityJsModule.InvokeVoidAsync("openPdfInNewTab", pdfBase64);
+
+    }
+
+    public byte[] GeneratePdf(IEnumerable<PdfElement> pdfElements)
+    {
+        _logger.LogInformation("PreparePdfDocument started");
+        IContainer? contentContainer = default;
+        var document = Document.Create(handler =>
+        {
+            handler.Page(page =>
+            {
+                page.Size(PageSizes.A4);
+                page.Header().Text("Header");
+                page.Footer().Text("Footer");
+                // margins, colors
+
+                contentContainer = page.Content();
+                foreach (var element in pdfElements)
+                {
+                    element.Draw(contentContainer);
+                }
+            });
+        });
+        
+        _logger.LogInformation("PreparePdfDocument finished");
+        return document.GeneratePdf();
+    }
+}
---
+++ wwwroot/scripts/pdfServiceUtility.js
@@ -0,0 +1,15 @@
+﻿export function openPdfInNewTab (pdfBase64) {
+    const pdfBlob = base64toBlob(pdfBase64, 'application/pdf');
+    const blobUrl = URL.createObjectURL(pdfBlob);
+    window.open(blobUrl, '_blank');
+}
+
+function base64toBlob(base64Data, contentType) {
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], { type: contentType });
+}
