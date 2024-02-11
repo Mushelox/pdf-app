3689f079272439bd4889ed865f7c9f6a5d08ff05
Jakub Muszyński <83725776+Mushelox@users.noreply.github.com>
2024-02-11 10:33:50 +0100

    Pdf builder supports reordering items in builder area.

--- Pages/PdfBuilder.razor.cs
+++ Pages/PdfBuilder.razor.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MudBlazor;
-
+using MudBlazor.Utilities;
 
 namespace PdfApp.Pages;
 
@@ -12,82 +12,37 @@ public partial class PdfBuilder
     private const string AREA_NAME_TEMPLATES = "templates";
     private const string AREA_NAME_BUILDER = "builder";
 
-    private List<DropItem> DefaultTemplateItems => new()
-    {
-        new DropItem { Index = 1, Name = "Element X", Identifier = "templates" },
-        new DropItem { Index = 2, Name = "Element Y", Identifier = "templates" },
-        new DropItem { Index = 3, Name = "Element Z", Identifier = "templates" }
-    };
+    private List<DropItem> _dropzoneItems = new();
+    private MudDropContainer<DropItem> dropContainerRef;
 
-    private List<DropItem> _dropzoneItems;
-
-    protected override void OnInitialized()
+    private void ItemUpdated(MudItemDropInfo<DropItem> dropInfo)
     {
-        _dropzoneItems = DefaultTemplateItems;
+        var droppedItem = dropInfo.Item!;
+        droppedItem.Identifier = dropInfo.DropzoneIdentifier;
+        droppedItem.Index = dropInfo.IndexInZone;
+
+        _dropzoneItems.UpdateOrder(dropInfo, item => item.Index, 1);
+
+        var sortedDropItems = _dropzoneItems.OrderBy(x => x.Index);
+        Logger.LogInformation("Item dropped. Order in dropzone: {data}", sortedDropItems.Select(x => $"\n{x.Index}: {x.Guid}"));
     }
 
-    private void ItemUpdated(MudItemDropInfo<DropItem> dropItemInfo)
-    {
-        var droppedItem = dropItemInfo.Item!;
-        // Logger.LogInformation("DropItemInfo.OrderInZone = {index}", dropItemInfo.IndexInZone);
-
-        if (droppedItem.Identifier == AREA_NAME_TEMPLATES)
-        {
-            droppedItem = new DropItem
-            {
-                Identifier = dropItemInfo.DropzoneIdentifier,
-                Index = dropItemInfo.IndexInZone,
-                Name = droppedItem.Name,
-                Guid = droppedItem.Guid
-            };
-            _dropzoneItems.Add(droppedItem);
-        }
-        else
-        {
-            droppedItem.Index = dropItemInfo.IndexInZone;
-            // foreach (var itemInCollection in ItemsInBuilderArea().Where(x => x.Index >= droppedItem.Index && x != droppedItem))
-            // {
-            //     if (itemInCollection.Guid.Equals(droppedItem.Guid))
-            //     {
-            //         Logger.LogInformation("GUIDs the same, skipping");
-            //         continue;
-            //     }
-            //
-            //     itemInCollection.Index++;
-            // }
-        }
-
-        var itemsWhichRequireIndexIncrease = ItemsInBuilderArea(droppedItem.Index, true).ToList();
-        itemsWhichRequireIndexIncrease.Remove(droppedItem);
-        
-        if (itemsWhichRequireIndexIncrease.Count > 0)
-            Logger.LogInformation("Items which require index update:{items}",
-                                  itemsWhichRequireIndexIncrease.Select(x => $"\n{x.Name}: currentIndex = {x.Index} || GUID = {x.Guid}"));
-        
-        foreach (var item in itemsWhichRequireIndexIncrease)
-            item.Index++;
-        
-
-        RestoreDefaultTemplates();
-
-        var orderedItemsInBuilderZone = ItemsInBuilderArea(ordered: true);
-        Logger.LogInformation("Item dropped. Items order:\n{itemsInOrder}",
-                              string.Join("\n", orderedItemsInBuilderZone.Select(item => $"{item.Index}: {item.Name}")));
-    }
-
-    private void RestoreDefaultTemplates()
-    {
-        _dropzoneItems.RemoveAll(x => x.Identifier == AREA_NAME_TEMPLATES);
-        _dropzoneItems.AddRange(DefaultTemplateItems);
-
-        // Logger.LogInformation("Items in templates area:\n{info}", 
-        //                       _dropzoneItems.Where(x => x.Identifier == AREA_NAME_TEMPLATES)
-        //                                     .Select(x => $"{x.Index}: {x.Name}"));
-    }
-    
     private void OnTemplateBlockClick(string cardType)
     {
-        //TODO update param from string to object which holds info about building block
+        var newItem = new DropItem
+        {
+            Name = cardType,
+            Identifier = AREA_NAME_BUILDER,
+            Index = _dropzoneItems.Count
+        };
+
+        _dropzoneItems.Add(newItem);
+
+        StateHasChanged();
+        dropContainerRef.Refresh();
+        Logger.LogInformation("Item dropped. Order in dropzone: {data}", _dropzoneItems
+                                                                         .OrderBy(x => x.Index)
+                                                                         .Select(x => $"\n{x.Index}: {x.Guid}"));
     }
 
 #region UTILITY
@@ -105,6 +60,11 @@ public partial class PdfBuilder
     }
 
 #endregion
+
+    private void OnGenerateClick()
+    {
+        throw new NotImplementedException();
+    }
 
     public class DropItem
     {
